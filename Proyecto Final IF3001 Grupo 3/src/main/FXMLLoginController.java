84b72c4e2dec8dbc966fb712fd1fa41cba08b4e6
Claudia Ulloa Campabadal <102692545+claudiaulloacampabadal@users.noreply.github.com>
2022-05-18 16:22:26 -0600

    Add files via upload

---
+++ Proyecto Final IF3001 Grupo 3/src/main/FXMLLoginController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class FXMLLoginController implements Initializable {
+
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Label lbClinica;
+    @FXML
+    private ComboBox<?> cbLogin;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+    }    
+    
+}
