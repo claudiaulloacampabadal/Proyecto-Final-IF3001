ee92e7825d9305f99ad589759f2fd333a66f31aa
Maria Celeste <Maria Celeste@LAPTOP-PKD9V84R>
2022-06-13 13:21:09 -0600

    Merge origin/main into main

--- Proyecto Final IF3001 Grupo 3/src/main/FXMLAddIllnesAndDiseaseController.java
+++ Proyecto Final IF3001 Grupo 3/src/main/FXMLAddIllnesAndDiseaseController.java
@@ -10,12 +10,20 @@ import domain.Sickness;
 import domain.TDA.ListException;
 import domain.TDA.SinglyLinkedList;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,9 +31,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
-import static main.FXMLIllnessAndDiseaseController.loadPage;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -37,7 +47,6 @@ public class FXMLAddIllnesAndDiseaseController implements Initializable {
     SinglyLinkedList illness;
    
     ArchiveTXT archives = new ArchiveTXT();
-    private SinglyLinkedList illness;
     private Alert alert;
 
     @FXML
