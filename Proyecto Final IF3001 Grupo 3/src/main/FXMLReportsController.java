2dda50f7453f3cbac800dea248074775a9c42304
Claudia Ulloa Campabadal <102692545+claudiaulloacampabadal@users.noreply.github.com>
2022-07-04 16:16:58 -0600

    Update FXMLReportsController.java

--- Proyecto Final IF3001 Grupo 3/src/main/FXMLReportsController.java
+++ Proyecto Final IF3001 Grupo 3/src/main/FXMLReportsController.java
@@ -4,18 +4,35 @@
  */
 package main;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import domain.PDF;
+import domain.Patient;
+import domain.Payment;
+import domain.TDA.CircularLinkedList;
+import domain.TDA.ListException;
+import domain.TDA.QueueException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import static main.FXMLPatientsReportsController.loadPage;
 
 /**
  * FXML Controller class
@@ -28,6 +45,8 @@ public class FXMLReportsController implements Initializable {
     private BorderPane bp;
     @FXML
     private Button generatePDF;
+    CircularLinkedList Patient;
+    PDF p = new PDF();
 
     /**
      * Initializes the controller class.
@@ -36,26 +55,106 @@ public class FXMLReportsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
-        public static void loadPage(URL ui, BorderPane bp){
+
+    private void btnGeneratePDF(ActionEvent event) throws DocumentException, BadElementException, IOException, QueueException, ListException {
+        
+//       p.generateReport(prueba, Prueba PDF, getData());
+        
+//            generaPDF();
+              
+
+    }
+
+    private ObservableList<java.util.List<String>> getData() throws QueueException, ListException {
+        //recordar agregar los datos
+//        Patient p = new Patient(0, lastname, firstname, birthday, email, adress);
+        ObservableList<java.util.List<String>> data = FXCollections.observableArrayList();
+
+        for (int i = 1; i <= Patient.size(); i++) {
+            Patient p = (Patient) Patient.getNext(Patient);
+            //Se le actualizan los cambios a la cola original en utility
+            util.Utility.setCircularLinkedList(Patient);
+
+            java.util.List<String> arrayList = new ArrayList<>();
+
+            arrayList.add(String.valueOf(p.getId()));
+            arrayList.add(util.Utility.format(p.getBirthday()));
+            arrayList.add(String.valueOf(p.getAdress()));
+            arrayList.add(String.valueOf(p.getEmail()));
+            arrayList.add(String.valueOf(p.getLastname()));
+
+            data.add(arrayList);
+        }
+            return data;
+        }
+        
+
+    public static void loadPage(URL ui, BorderPane bp) {
         Parent root = null;
         try {
-            root = FXMLLoader.load(ui); 
+            root = FXMLLoader.load(ui);
         } catch (IOException ex) {
             Logger.getLogger(FXMLMainMenuController.class.getName());
         }
         //cleaning nodes
         //bp.setTop(null);
-        bp.setCenter(null); 
+        bp.setCenter(null);
         //bp.setBottom(null); 
         //bp.setLeft(null);
         //bp.setRight(null);
         bp.setCenter(root);
     }
+//
+//    @FXML
+//        public void generaPDF() throws FileNotFoundException, DocumentException, BadElementException, IOException {
+//        ArrayList arrayTest = new ArrayList();
+//        FileOutputStream archive = new FileOutputStream("C:\\Users\\Usuario\\Desktop\\Prueba\\Pruebas.pdf");
+//        Document doc = new Document();
+//
+//        arrayTest.add("Hamburguesas");
+//        arrayTest.add("Pizza");
+//        arrayTest.add("Empanadas");
+//        arrayTest.add("Burritos");
+//        arrayTest.add("Papas");
+//
+//
+//        PdfWriter.getInstance(doc, archive);
+//        doc.open();
+//
+//        Paragraph title = new Paragraph("Lista de Comida\n\n", FontFactory.getFont("arial", 22, Font.BOLD, BaseColor.BLACK));
+//
+//        title.setAlignment(Paragraph.ALIGN_CENTER);
+//
+//        Image img = Image.getInstance("C:\\Users\\Usuario\\Desktop\\Prueba\\icon.png");
+//        img.setAlignment(Image.ALIGN_CENTER);
+//        img.setBorder(Image.BOX);
+//        img.setBorderWidth(0);
+//        img.setBorderColor(BaseColor.WHITE);
+//        img.scaleToFit(1100, 100); // tamaño 
+//        doc.add(img);
+//        doc.add(title);
+//        Paragraph products = new Paragraph("Comidas" + "\n\n", FontFactory.getFont("arial", 18, Font.NORMAL, BaseColor.BLACK));
+//        products.setAlignment(Paragraph.ALIGN_CENTER);
+//        doc.add(products);
+//
+//        for (int i = 0; i < arrayTest.size(); i++) {
+//
+//            Paragraph food = new Paragraph(arrayTest.get(i) + "\n\n", FontFactory.getFont("arial", 18, Font.NORMAL, BaseColor.BLACK));
+//            food.setAlignment(Paragraph.ALIGN_CENTER);
+//            doc.add(food);
+//            PdfPTable tablefood = new PdfPTable(1); //El numero adentro del paréntesis es la cantidad de columnas
+//            doc.add(tablefood);
+//
+//        }
+//
+//        doc.close();
+//        
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle("Confirmación");
+//        alert.setContentText("Se ha generado el pdf");
+//        alert.show();
+//
+//    }
 
-         private void btnGeneratePDF(ActionEvent event) {
-        // PDF pdf =  new PDF();
-         
-
-    }
 }
+
